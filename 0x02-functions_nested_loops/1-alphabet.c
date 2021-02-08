091b7555ab9dd3015343d54be5b130949e276c57
Lvmvr22 <lamarnyairo@gmail.com>
2021-02-08 16:12:48 +0300

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_alphabet - prints alphabet in lowercase
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+	_putchar('\n');
+}
