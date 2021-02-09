ad488b31d5ae65ba501b4b2f0e261a3f97ff85b3
Lvmvr22 <lamarnyairo@gmail.com>
2021-02-09 14:52:41 +0300

    fixinf errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 
 /**
- *print_last_digit - prints last digit of a number
- *@b:number entered
- *Return: returns the last digit of n
+ *print_last_digit - Prints last digit of a number
+ *@x:Number entered
+ *Return: Returns the last digit of n
  */
 
-int print_last_digit(int b)
+int print_last_digit(int x)
 {
 	int last;
 
-	if (b < 0)
+	if (x < 0)
 	{
 		last = (x % 10) * -1;
 	}
 	else
 	{
-		last = b % 10;
+		last = x % 10;
 	}
 	_putchar (last + '0');
 	return (last);
