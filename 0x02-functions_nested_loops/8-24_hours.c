2fd102d70d0e4e6f16f475ba81508ea3ef0c903d
Lvmvr22 <lamarnyairo@gmail.com>
2021-02-09 15:26:30 +0300

    prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,22 @@
 #include "holberton.h"
-
 /**
- *jack_bauer - Prints every minute of the day
+ *jack_bauer -  count down 24hrs
  *
- *Return:0
+ *Return: void.
  */
-
-void jack_bauer(void)
+void  jack_bauer(void)
 {
-int a, b, c, d;
-
-for (a = 0; a <= 2; a++)
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
 {
-for (b = 0; b <= 9; b++)
+for (minute = 0; minute <= 59; minute++)
 {
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-if (a == 2 && b > 3)
-{
-break;
-}
-_putchar(a + '0');
-_putchar(b + '0');
+_putchar('0' + (hour / 10));
+_putchar('0' + (hour % 10));
 _putchar(':');
-_putchar(c + '0');
-_putchar(d + '0');
+_putchar('0' + (minute / 10));
+_putchar('0' + (minute % 10));
 _putchar('\n');
-}
-}
 }
 }
 }
