771003c953a2835b5a777ca3cc01be12407797c2
Lvmvr22 <lamarnyairo@gmail.com>
2021-02-17 14:39:56 +0300

    fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,31 +1,35 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 /**
- * _atoi-converts string to int
- * @s:parameter
+ * main - the program generate random 9 digits
  *
- * Return:integer
+ * Return: 0 if successful
  */
 
-int _atoi(char *s)
+int main(void)
 {
-int i, val, sign;
+int i = 0;
+int n = 0;
+int sum = 0;
+char password[100];
 
-val = 0;
-sign = 1;
-
-for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
+char al[63] = "0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+srand(time(NULL));
+while (sum < (2772 - 122))
 {
-if (s[i] == '-')
-sign = sign * -1;
+
+n = rand() % 62;
+password[i] = al[n];
+sum = sum + password[i];
+i++;
 }
 
-for (i = 0; s[i] != 0; i++)
-{
-if (s[i] >= '0' && s[i] <= '9')
-val = val * 10 + sign * (s[i] - '0');
-if (val != 0 && !(s[i] >= '0' && s[i] <= '9'))
-return (val);
-}
-return (val);
+n = 2772 - sum;
+password[i] = n;
+password[++i] = '\0';
+printf("%s\n", password);
+return (0);
 }
