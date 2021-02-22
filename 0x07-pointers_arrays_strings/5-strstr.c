806285071ab184fcaf4fa03f36685c9ecd5555b1
Lvmvr22 <lamarnyairo@gmail.com>
2021-02-22 13:52:27 +0300

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: pointer.
+ * @needle: character.
+ * Return: Pointer to the beginning of the located substring or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int l, m, n;
+
+	for (l = 0; haystack[l] != '\0'; l++)
+	{
+		for (n = l, m = 0; needle[m] != '\0'; m++, n++)
+		{
+			if (needle[m] != haystack[n] || haystack[n] == '\0')
+			{
+				break;
+			}
+		}
+		if (needle[m] == '\0')
+		{
+			return (haystack + l);
+		}
+	}
+	return (0);
+}
