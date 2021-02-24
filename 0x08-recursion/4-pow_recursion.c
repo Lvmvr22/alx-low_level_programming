11573ae8bdd0434e4138f4d17a2904332951e8cc
Lvmvr22 <lamarnyairo@gmail.com>
2021-02-24 16:59:33 +0300

    returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion - Entry Point. Returns the power of an integer to another
- * @l: base integer
- * @n: exponent int
- * Return: Returns the value of l to power y
- */
+* _pow_recursion - Entry Point. Returns the power of an integer to another
+* @x: Base integer
+* @y: Exponent Integer
+* Return: Returns x to power y
+*/
 
-int _pow_recursion(int l, int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
 		return (-1);
 
-	else if (n == 0)
+	else if (y == 0)
 		return (1);
 
-	return (l * _pow_recursion(l, n - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
