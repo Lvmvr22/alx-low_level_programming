fcca1e48785e7d5467583cbe4748e4d2b9d4f6fb
Lvmvr22 <lamarnyairo@gmail.com>
2021-03-04 10:05:16 +0300

    allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*malloc_checked - allocates memory using malloc
+*@b: memory size
+*Return: void pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (!ptr)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
