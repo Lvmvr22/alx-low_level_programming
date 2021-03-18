e915473897cb2d6b7780e0c3e435dec06e8f9624
Lvmvr22 <lamarnyairo@gmail.com>
2021-03-18 22:23:42 +0300

    linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void beforeMain(void)__attribute__((constructor));
+/**
+ * beforeMain - prints before main function is executed
+ */
+
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
